3a406509ec091501ecf91d9752b86f8359eef789
= <k.dean.jordan@gmail.com>
2020-05-15 15:25:12 -0700

    add dockerignore

--- server.ts
+++ server.ts
@@ -28,59 +28,59 @@ import * as c from './config';
     res.send('USER FEED RUNNING')
   });
   
-  // Get all feed items
-  // app.get('/', async (req: Request, res: Response) => {
-  //   // res.send('FEED UP and healthy')
-  //   const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
-  //   items.rows.map((item) => {
-  //     if (item.url) {
-  //       item.url = AWS.getGetSignedUrl(item.url);
-  //     }
-  //   });
-  //   res.send(items);
-  // });
+ // Get all feed items
+  app.get('/', async (req: Request, res: Response) => {
+    // res.send('FEED UP and healthy')
+    const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
+    items.rows.map((item) => {
+      if (item.url) {
+        item.url = AWS.getGetSignedUrl(item.url);
+      }
+    });
+    res.send(items);
+  });
   
-  // // Get a feed resource
-  // app.get('/:id',
-  //     async (req: Request, res: Response) => {
-  //       const {id} = req.params;
-  //       const item = await FeedItem.findByPk(id);
-  //       res.send(item);
-  //     });
+  // Get a feed resource
+  app.get('/:id',
+      async (req: Request, res: Response) => {
+        const {id} = req.params;
+        const item = await FeedItem.findByPk(id);
+        res.send(item);
+      });
   
-  // // Get a signed url to put a new item in the bucket
-  // app.get('/signed-url/:fileName',
-  //     requireAuth,
-  //     async (req: Request, res: Response) => {
-  //       const {fileName} = req.params;
-  //       const url = AWS.getPutSignedUrl(fileName);
-  //       res.status(201).send({url: url});
-  //     });
+  // Get a signed url to put a new item in the bucket
+  app.get('/signed-url/:fileName',
+       requireAuth,
+      async (req: Request, res: Response) => {
+        const {fileName} = req.params;
+        const url = AWS.getPutSignedUrl(fileName);
+        res.status(201).send({url: url});
+      });
   
-  // // Create feed with metadata
-  // app.post('/',
-  //     requireAuth,
-  //     async (req: Request, res: Response) => {
-  //       const caption = req.body.caption;
-  //       const fileName = req.body.url; // same as S3 key name
+  // Create feed with metadata
+  app.post('/',
+      requireAuth,
+      async (req: Request, res: Response) => {
+        const caption = req.body.caption;
+        const fileName = req.body.url; // same as S3 key name
   
-  //       if (!caption) {
-  //         return res.status(400).send({message: 'Caption is required or malformed.'});
-  //       }
+        if (!caption) {
+          return res.status(400).send({message: 'Caption is required or malformed.'});
+        }
   
-  //       if (!fileName) {
-  //         return res.status(400).send({message: 'File url is required.'});
-  //       }
+        if (!fileName) {
+          return res.status(400).send({message: 'File url is required.'});
+        }
   
-  //       const item = await new FeedItem({
-  //         caption: caption,
-  //         url: fileName,
-  //       });
+        const item = await new FeedItem({
+          caption: caption,
+          url: fileName,
+        });
   
-  //       const savedItem = await item.save();
+        const savedItem = await item.save();
   
-  //       savedItem.url = AWS.getGetSignedUrl(savedItem.url);
-  //       res.status(201).send(savedItem);
-  //     });
+        savedItem.url = AWS.getGetSignedUrl(savedItem.url);
+        res.status(201).send(savedItem);
+      });
 })();
 
