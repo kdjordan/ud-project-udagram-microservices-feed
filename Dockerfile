590f278772cded272ef7e6b3bdb28404d57c1cd9
= <k.dean.jordan@gmail.com>
2020-05-16 12:53:23 -0700

    fixed deploy methods

--- Dockerfile
+++ Dockerfile
@@ -7,17 +7,11 @@ WORKDIR /usr/src/app
 # Install app dependencies by copying
 # package.json and package-lock.json
 COPY package*.json ./
+RUN npm ci 
 
-# Install dependencies
-RUN npm install
-
-# Copy app source
+# Bundle app source
 COPY . .
 
-# Bind the port that the image will run on
-EXPOSE 8080
+EXPOSE 80
 
-RUN npm run dev
-
-# Define the Docker image's behavior at runtime
-CMD ["node", "server.ts"]
+CMD [ "npm", "run", "dev" ]
--- config.ts
+++ config.ts
@@ -1,18 +1,35 @@
 export const config = {
     "postgress": {
-      "username": process.env.POSTGRES_USERNAME,
-      "password": process.env.POSTGRES_PASSWORD,
-      "database": process.env.POSTGRES_DATABASE,
-      "host": process.env.POSTGRES_HOST,
-      "dialect": process.env.POSTGRES_DIALECT
+      "username": 'udagramjordan',
+      "password": 'udagramjordanpass',
+      "database": 'udagramjordan',
+      "host": 'udagramjordan.cqk1udiwxavz.us-east-2.rds.amazonaws.com',
+      "dialect": 'postgres'
     },
     "aws": {
-      "aws_region": process.env.AWS_REGION,
-      "aws_profile": process.env.AWS_PROFILE,
-      "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+      "aws_region": 'us-east-2',
+      "aws_profile": 'default',
+      "aws_media_bucket": 'udagramjordan-dev'
     },
     "jwt" : {
-      "secret": process.env.SECRET_KEY
+      "secret": 'GoDucks'
     }
   }
+// export const config = {
+//     "postgress": {
+//       "username": process.env.POSTGRES_USERNAME,
+//       "password": process.env.POSTGRES_PASSWORD,
+//       "database": process.env.POSTGRES_DATABASE,
+//       "host": process.env.POSTGRES_HOST,
+//       "dialect": process.env.POSTGRES_DIALECT
+//     },
+//     "aws": {
+//       "aws_region": process.env.AWS_REGION,
+//       "aws_profile": process.env.AWS_PROFILE,
+//       "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+//     },
+//     "jwt" : {
+//       "secret": process.env.SECRET_KEY
+//     }
+//   }
   
