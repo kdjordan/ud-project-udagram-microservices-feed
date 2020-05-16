c31c0c8ac281881eb8344e83d6c27b9f7de00967
= <k.dean.jordan@gmail.com>
2020-05-16 12:55:27 -0700

    config fix

--- config.ts
+++ config.ts
@@ -1,35 +1,19 @@
+
 export const config = {
     "postgress": {
-      "username": 'udagramjordan',
-      "password": 'udagramjordanpass',
-      "database": 'udagramjordan',
-      "host": 'udagramjordan.cqk1udiwxavz.us-east-2.rds.amazonaws.com',
-      "dialect": 'postgres'
+      "username": process.env.POSTGRES_USERNAME,
+      "password": process.env.POSTGRES_PASSWORD,
+      "database": process.env.POSTGRES_DATABASE,
+      "host": process.env.POSTGRES_HOST,
+      "dialect": process.env.POSTGRES_DIALECT
     },
     "aws": {
-      "aws_region": 'us-east-2',
-      "aws_profile": 'default',
-      "aws_media_bucket": 'udagramjordan-dev'
+      "aws_region": process.env.AWS_REGION,
+      "aws_profile": process.env.AWS_PROFILE,
+      "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
     },
     "jwt" : {
-      "secret": 'GoDucks'
+      "secret": process.env.SECRET_KEY
     }
   }
-// export const config = {
-//     "postgress": {
-//       "username": process.env.POSTGRES_USERNAME,
-//       "password": process.env.POSTGRES_PASSWORD,
-//       "database": process.env.POSTGRES_DATABASE,
-//       "host": process.env.POSTGRES_HOST,
-//       "dialect": process.env.POSTGRES_DIALECT
-//     },
-//     "aws": {
-//       "aws_region": process.env.AWS_REGION,
-//       "aws_profile": process.env.AWS_PROFILE,
-//       "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
-//     },
-//     "jwt" : {
-//       "secret": process.env.SECRET_KEY
-//     }
-//   }
   
