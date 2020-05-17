9b6d3a95b70ad471f4e3dee8cac1cf14a14d84f8
= <k.dean.jordan@gmail.com>
2020-05-16 17:02:02 -0700

    removed console.log

--- sequelize.ts
+++ sequelize.ts
@@ -3,7 +3,7 @@ import {Sequelize} from 'sequelize-typescript';
 import {config} from './config';
 
 const c = config.postgress
-console.log(c)
+
 
 export const sequelize = new Sequelize({
   'username': c.username,
